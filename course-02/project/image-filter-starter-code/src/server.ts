b1b47f891aa82bfc0b3a253fafb34058ce4efb34
HWBN1 <m.hamed@yodawy.com>
2022-09-01 18:47:53 +0200

    add a endpoint to filter images

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -18,14 +18,14 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // endpoint to filter an image from a public url.
   // IT SHOULD
   //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
+  //    1. validate the image_url query => done
+  //    2. call filterImageFromURL(image_url) to filter the image => done
+  //    3. send the resulting file in the response => done
+  //    4. deletes any files on the server on finish of the response => done
   // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
+  //    image_url: URL of a publicly accessible image => done
   // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful] => done
 
   /**************************************************************************** */
 
@@ -37,6 +37,26 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
+  app.get( "/filteredimage?:image_url", async ( req, res ) => {
+    // read the url
+    let { image_url } = req.query;
+    // validate the query
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`image_url is required`);
+    }
+    // filter the image
+    const filtered_image = filterImageFromURL(image_url);
+    // send the resulting file in the response
+    filtered_image.then((filteredpath) => {
+      // send the file as an octet stream.
+      res.sendFile(filteredpath);
+      // clean up tmp folders
+      setTimeout(() => {
+        deleteLocalFiles([filteredpath])
+      }, 5000);
+    })
+  } );
 
   // Start the Server
   app.listen( port, () => {
